81be1b886ea8f3addc64bc808fb003d3b73c3e86
Changha <kch31411@kaist.ac.kr>
2021-03-10 16:38:02 +0900

    Promotion & seller model 생성
    필수 요소들을 담고 있는 가장 기본적인 모델을 생성함.
    makemigrations 명령어를 이용해서 migration file도 생성.

--- back-end/acon3d/settings.py
+++ back-end/acon3d/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'promotion.apps.PromotionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- back-end/promotion/models.py
+++ back-end/promotion/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Promotion(models.Model):
+    title = models.CharField(max_length=255)
+    min_seller = models.IntegerField()
+    max_seller = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+
+class Seller(models.Model):
+    brand_id = models.CharField(max_length=10)
