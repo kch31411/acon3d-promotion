54b9a043b82273ee24312daa792899e9ae28e38d
Changha <kch31411@kaist.ac.kr>
2021-03-10 21:13:57 +0900

    프로모션의 상태를 표시
    신청 가능, 인원 초과, 인원 부족으로 취소, 프로모션 진행 중, 프로모션 마감
    상태를 화면에 표시

--- front-end/src/components/PromotionItem.js
+++ front-end/src/components/PromotionItem.js
@@ -28,6 +28,24 @@ function PromotionItem(props) {
     const [promotion, setPromotion] = useState(props.promotion)
     const {id, title, min_seller, max_seller, start_date, end_date, participants} = promotion;
 
+    // TODO: 아래 상태 정보는 서버에서 보내주는 것이 좋을 듯.
+    // TODO: 취소 되었음을 유저에게 알리기
+    const status = (promotion) => {
+        if (new Date() < new Date(start_date)) {
+            if (participants.length < max_seller) {
+                return "신청 가능"
+            } else {
+                return "인원 초과"
+            }
+        } else if (participants.length < min_seller) {
+            return "인원 부족으로 취소됨"
+        } else if (end_date && new Date() > new Date(end_date)) {
+            return "마감됨"
+        } else {
+            return "진행중"
+        }
+    }
+
     const apply = () => {
         const config = {
             method: 'POST',
@@ -49,6 +67,7 @@ function PromotionItem(props) {
     return (
         <Paper className={classes.container}>
             <Typography variant="h6">{title}</Typography>
+            <Typography variant="subtitle2" color='error'>{status(promotion)}</Typography>
             <Typography variant="subtitle2">프로모션 기간: {start_date} ~ {end_date}</Typography>
             <Typography variant="subtitle2">최소 인원 {min_seller}명 | 최대 인원 {max_seller}명</Typography>
             <Typography variant="subtitle2">참가 ({participants.length}명): {participants}</Typography>
